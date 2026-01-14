26775f232db62461cea0f1d9f3b7c1cfec4bb868
manasav1212 <manasav@unm.edu>
2026-01-13 18:30:25 -0700

    Backend code to establish connection with database

--- Advanced To-Do List/Backend/app.js
+++ Advanced To-Do List/Backend/app.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const {db} = require("pg");
+const { Pool } = require("pg");
 
 const app = express();
 
-app.use(cors);
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
 
 const port = 8080;
 
-const db = new db({
+const db = new Pool({
     user: "postgres",
     host: "localhost",
     database: "To-Do_List",
-    password: "Password",
+    password: "",
     port: 5432
 });
 
 app.get("/todo", async (req, res) =>{
     try
     {
-        const result = await db.query("SELECT * FROM 'Task_List' ORDER BY id ASC");
+        const result = await db.query('SELECT * FROM "Task_List" ORDER BY id ASC');
         res.json(result.rows);
     }
     catch(error)
@@ -29,11 +29,13 @@ app.get("/todo", async (req, res) =>{
     }
 });
 
-app.post("/todo/id", async (req, res) =>{
+app.post("/todo/add", async (req, res) =>{
     try
     {
-        const {entry} = req.body;
-        const result = await db.query("INSERT INTO 'Task_List' () VALUES ");
+        const {name} = req.body;
+        const query = 'INSERT INTO "Task_List" (name) VALUES ($1) RETURNING *';
+        const result = await db.query(query, [name]);
+        res.json(result.rows[0]);
     }
     catch(error)
     {
@@ -42,10 +44,13 @@ app.post("/todo/id", async (req, res) =>{
 
 });
 
-app.put("/todo/id", async (req, res) =>{
+app.put("/todo/edit/:id", async (req, res) =>{
     try
     {
-        const result = await db.query("UPDATE 'Task_List' SET ");
+        const {name} = req.body;
+        const {id} = req.params; 
+        const result = await db.query('UPDATE "Task_List" SET name = $1 WHERE id= $2', [name, id]);
+        res.json({message: "Edit successful"});
     }
     catch(error)
     {
@@ -53,10 +58,25 @@ app.put("/todo/id", async (req, res) =>{
     }
 });
 
-app.delete("/todo/id", async (req, res) =>{
+app.put("/todo/done/:id", async (req, res) =>{
     try
     {
-        const result = await db.query("DELETE FROM 'Task_List' WHERE");
+        const {id} = req.params; 
+        const result = await db.query('UPDATE "Task_List" SET status = true WHERE id= $1', [id]);
+        res.json({message: "Marked as Done"});
+    }
+    catch(error)
+    {
+        res.status(500).json({error:error.message});
+    }
+});
+
+app.delete("/todo/:id", async (req, res) =>{
+    try
+    {
+        const {id} = req.params;
+        const result = await db.query('DELETE FROM "Task_List" WHERE id = $1', [id]);
+        res.json({message: "Deleted Successfully"});
     }
     catch(error)
     {
--- Advanced To-Do List/advancedToDo.js
+++ Advanced To-Do List/advancedToDo.js
@@ -1,3 +1,6 @@
+const db_URL = "http://localhost:8080/todo";
+
+window.onload = loadTasks;
 
 function clearFilter()
 {
@@ -45,14 +48,28 @@ function handleFilterSelection(value)
         pendingFilter();
 }
 
-function taskCompleted(event)
+async function taskCompleted(event)
 {
-    
+    const element = event.target;
+    const div = element.closest("div");
+    const id = div.dataset.id;
+    const result = await fetch(`${db_URL}/done/${id}`,{
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({status:true})
+    });
 }
 
-function taskPending(event)
+async function taskPending(event)
 {
-
+    const element = event.target;
+    const div = element.closest("div");
+    const id = div.dataset.id;
+    const result = await fetch(`${db_URL}/done/${id}`,{
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({status:false})
+    });
 }
 
 function checkboxChange(event)
@@ -79,31 +96,49 @@ function editTask(event)
 
     div.replaceChild(editInput, span);
     editInput.focus();
-    editInput.addEventListener("keydown", function(event){
+    editInput.addEventListener("keydown", async function(event){
         if(event.key === "Enter")
         {
+            const result = await fetch(`${db_URL}/edit/${id}`,{
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body:  JSON.stringify({name:editInput.value})
+            });
             span.textContent = editInput.value;
             div.replaceChild(span, editInput);
         }
     });
-    editInput.addEventListener("blur",function(){
+    editInput.addEventListener("blur",async function(){
+        const result = await fetch(`${db_URL}/edit/${id}`,{
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({name:editInput.value})
+        });
         span.textContent = editInput.value;
         div.replaceChild(span, editInput);
     });
 }
 
-function deleteTask(event)
+async function deleteTask(event)
 {
     const element = event.target;
-    element.closest("div").remove();
+    const div = element.closest("div");
+    const id = div.dataset.id;
+    await fetch(`${db_URL}/${id}`,{
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" }
+    });
+    div.remove();
 }
 
-function getTask(task)
+function getTask(id, task, status = false)
 {
     let taskDiv = document.createElement("div");
+    taskDiv.dataset.id = id;
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked - status;
     checkbox.addEventListener("change", checkboxChange);
 
     const taskDetails = document.createElement("span");
@@ -127,13 +162,30 @@ function getTask(task)
     return taskDiv;
 }
 
-function createNewTask(task)
+async function loadTasks()
+{
+    const result = await fetch(db_URL);
+    const Tasks = await result.json();
+    for(let task of Tasks)
+    {
+        const taskDiv = getTask(task.id, task.name, task.status);
+        document.getElementById("taskList").appendChild(taskDiv);
+    }
+}
+
+async function createNewTask(task)
 {
     if(task === "" || task === " ")
         alert("Invalid Task, Task cannot be empty!");
     else
     {
-        const taskDiv = getTask(task);
+        const res = await fetch(`${db_URL}/add`,{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({name: task})
+        });
+        const result = await res.json();
+        const taskDiv = getTask(result.id, result.name);
         document.getElementById("taskList").appendChild(taskDiv);
     }
 }
